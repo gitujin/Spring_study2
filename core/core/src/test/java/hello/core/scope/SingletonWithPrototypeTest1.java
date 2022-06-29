a7c3247ba7278e828a0918a38592b5afc2dd100b
gitujin <syj991114@gmail.com>
2022-06-29 20:33:20 +0900

    마지막 마무리

--- core/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,16 +1,18 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SingletonWithPrototyoeTest1 {
+public class SingletonWithPrototypeTest1 {
 
     @Test
     void prototypeFind(){
@@ -35,19 +37,23 @@ public class SingletonWithPrototyoeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton") //ClientBean은 Singleton
     static class ClientBean{
-        private final PrototypeBean prototypeBean; //생성시점에 주입
-
+//        private final PrototypeBean prototypeBean; //생성시점에 주입
+        
         @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
+/*        @Autowired
         public ClientBean(PrototypeBean prototypeBean){
             this.prototypeBean = prototypeBean;
-        }
+        }*/
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvider.get(); //찾아주는 기능만 제공. 스프링에서 필요한 것만..
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
